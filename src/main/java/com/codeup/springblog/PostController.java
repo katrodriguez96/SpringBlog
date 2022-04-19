f7cc0e15708626c49636479c9faded9cb4b85058
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2022-04-19 12:38:30 -0500

    completed view exercise

--- src/main/java/com/codeup/springblog/PostController.java
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -1,19 +1,28 @@
 package com.codeup.springblog;
 
+import model.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PostController {
     @GetMapping("/posts")
-    @ResponseBody
-    public String posts(){
-        return "posts index page";
+    public String posts(Model model){
+        List<Post> posts = new ArrayList<>();
+        posts.add(new Post("post 1", "post 1 content"));
+        posts.add(new Post("post 2", "post 2 content"));
+        model.addAttribute("posts", posts);
+        return "posts/index";
     }
-    @RequestMapping(path="/posts/{id}", method = RequestMethod.GET)
-    @ResponseBody
-    public String individualPost(@PathVariable int id){
-        return "view an individual post with an id of " + id;
+    @GetMapping("/posts/{id}")
+    public String individualPost(@PathVariable int id, Model model){
+        Post post = new Post("title", "content");
+        model.addAttribute("post", post);
+        return "posts/show";
     }
     @RequestMapping(path="/posts/create", method = RequestMethod.GET)
     @ResponseBody
